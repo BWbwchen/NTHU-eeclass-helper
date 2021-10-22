1aaceabe2515a489db4977bb99e80e2c6f75b6da
BWbwchen <tim.chenbw@gmail.com>
2021-10-22 11:17:50 +0800

    decompress all file and solve encoding problem

--- decompress.sh
+++ decompress.sh
@@ -1,1 +1,14 @@
+mkdir hw
+find . -iname '*.zip' -exec sh -c 'mv {} hw/' ';'
+
+# decompress the whole zip
 find . -iname '*.zip' -exec sh -c 'unzip -o -d "${0%.*}" "$0"' '{}' ';'
+
+# rename, only remain student id
+for file in hw/*/*;
+do
+    mv "${file}" "${file% (*}"
+done
+
+# decompress each student file
+find hw/*/* -iname '*.zip' -exec sh -c 'unzip -o -d "${0%.*}" "$0"' '{}' ';'
